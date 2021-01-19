496f3722c22c541b894c38fe4fefa669a95c2242
wijaysali <wijaysali@gmail.com>
2021-01-19 13:08:40 +0700

    added testSuite 1

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>16e503e3-dea7-4712-8a3b-3fdb4546fa87</testSuiteGuid>
+</TestSuiteEntity>
